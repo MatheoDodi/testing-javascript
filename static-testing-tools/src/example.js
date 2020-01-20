f80f0741d000af4f73e43bcbc2f9cc5662b25d25
Matthew Dodi <matthew.dodi@gmail.com>
2020-01-19 19:44:22 -0800

    Installed and added prettier config

--- static-testing-tools/src/example.js
+++ static-testing-tools/src/example.js
@@ -1,12 +1,9 @@
-'use strict'
+const username = 'freddy';
+typeof username === 'string';
 
-const username = 'freddy'
-typeof username === 'strng'
-
-if (!'serviceWorker' in navigator) {
+if (!('serviceWorker' in navigator)) {
   // you have an old browser :-(
 }
 
-const greeting = 'hello'
-console.log('${greeting} world!')
-[1, 2, 3].forEach(x => console.log(x))
+const greeting = 'hello';
+console.log(`${greeting} world!`)[(1, 2, 3)].forEach(x => console.log(x));
